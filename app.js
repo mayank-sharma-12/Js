527b842c9333bdad05632877f606f831031c2299
Mayank Sharma <mayank.consurgys@gmail.com>
2025-05-31 17:35:42 +0530

    If condition practice

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 // Template Literals
 
-let pencilPrice = 30;
-let sharpnerPrice = 10;
+// let pencilPrice = 30;
+// let sharpnerPrice = 10;
 
-console.log(`The price of Pencil and Sharpner will be : ${pencilPrice + sharpnerPrice} Rupees`);
+// console.log(`The price of Pencil and Sharpner will be : ${pencilPrice + sharpnerPrice} Rupees`);
+
+// Conditional Statements (if)
+
+console.log("Before if statement");
+
+let age = 26;
+
+if(age > 18){
+    console.log("You can vote");
+}
+
+let myName = "Mayank";
+if(myName == "Mayank"){
+    console.log(`Welcome, ${myName}`);
+}
+
+console.log("After if statement");
