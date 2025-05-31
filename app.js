b97087a870f19cc08a055e92211ec860a9efa56d
Mayank Sharma <mayank.consurgys@gmail.com>
2025-05-31 18:04:37 +0530

    Else if condition statement practice

--- app.js
+++ app.js
@@ -1,37 +1,49 @@
 // Template Literals
 
-// let pencilPrice = 30;
-// let sharpnerPrice = 10;
+let pencilPrice = 30;
+let sharpnerPrice = 10;
 
-// console.log(`The price of Pencil and Sharpner will be : ${pencilPrice + sharpnerPrice} Rupees`);
+console.log(`The price of Pencil and Sharpner will be : ${pencilPrice + sharpnerPrice} Rupees`);
 
 // Conditional Statements (if)
 
-// console.log("Before if statement");
+console.log("Before if statement");
 
-// let age = 26;
+let age = 26;
 
-// if(age > 18){
-//     console.log("You can vote");
-// }
+if(age > 18){
+    console.log("You can vote");
+}
 
-// let myName = "Mayank";
-// if(myName == "Mayank"){
-//     console.log(`Welcome, ${myName}`);
-// }
+let myName = "Mayank";
+if(myName == "Mayank"){
+    console.log(`Welcome, ${myName}`);
+}
 
-// console.log("After if statement");
+console.log("After if statement");
 
-// Traffic Signal Task by (If statement)
+// Traffic Signal Task by (If statement / else if statement)
 
 let color = "yellow";
 
-if(color == "red"){
+if(color === "red"){
     console.log("Stop");
-}
-if(color == "yellow"){
+}else if(color === "yellow"){
     console.log("Slow Down");
-}
-if(color == "green"){
+}else if(color === "green"){
     console.log("Go");
+}
+
+// Conditional Statements (else if)
+
+let marks = 62;
+
+if(marks >= 80){
+    console.log("Grade A+");
+}else if(marks >= 60){
+    console.log("Grade A");
+}else if(marks >= 33){
+    console.log("Grade B");
+}else if(marks < 33){
+    console.log("Grade F");
 }
