2993eab26857a718017b3520caa98d2c8f3e86b3
Mayank Sharma <mayank.consurgys@gmail.com>
2025-06-08 16:00:38 +0530

    Arrays Methods

---
+++ array.js
@@ -0,0 +1,16 @@
+//Arrays are mutable
+
+let cars = ["Tata", "Mahindra", "Farrari"];
+
+cars[0] = "BMW";
+
+//Array Push, Pop, Unshift and Shift
+
+let students = ["Mayank", "Pawan", "Bhanu"];
+students.push("Rohit"); // ['Mayank', 'Pawan', 'Bhanu', 'Rohit']
+
+students.pop(); //['Mayank', 'Pawan', 'Bhanu']
+
+students.unshift("Rohit"); //['Rohit', 'Mayank', 'Pawan', 'Bhanu']
+
+students.shift(); //['Mayank', 'Pawan', 'Bhanu']
