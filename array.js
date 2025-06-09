ca5b671ec403d5aff4aebaae118cd9f5e9227337
Mayank Sharma <mayank.consurgys@gmail.com>
2025-06-09 11:32:38 +0530

    Adding Concate and Reverse method

--- array.js
+++ array.js
@@ -14,3 +14,21 @@ students.pop(); //['Mayank', 'Pawan', 'Bhanu']
 students.unshift("Rohit"); //['Rohit', 'Mayank', 'Pawan', 'Bhanu']
 
 students.shift(); //['Mayank', 'Pawan', 'Bhanu']
+
+//indexof[] and includes
+
+students.indexOf("Bhanu"); // Output:- 1
+students.includes("Mayank"); // True
+
+// Concate & Reverse
+
+let fruits = ["Mango", "Apple", "Orange"];
+let veg = ["Potato", "Tomato", "Pumpkin"];
+
+let allconcate = fruits.concat(veg); //['Mango', 'Apple', 'Orange', 'Potato', 'Tomato', 'Pumpkin']
+veg.concat
+console.log(allconcate);
+
+let allReverse = fruits.reverse(); //['Orange', 'Apple', 'Mango']
+console.log(allReverse);
+
