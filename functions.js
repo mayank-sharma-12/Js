c17796a5b80c9bcc2bdb873d17cbc64e12bc0995
Mayank Sharma <mayank.consurgys@gmail.com>
2025-06-23 16:34:38 +0530

    OddEvenTest by high order function

--- functions.js
+++ functions.js
@@ -144,3 +144,21 @@ function highOrderFunction(func, count){
     }
 
 highOrderFunction(greets, 200);
+
+//Multiple step multiorderfunctions
+
+function oddEvenTest(request){
+    if(request == "odd"){
+        return function(n){
+            console.log(!(n % 2 == 0));
+        }
+    } else if(request == "even"){
+        return function(n){
+            console.log(n % 2 == 0);
+        }
+    } else{
+        console.log("wrong request");
+    }
+}
+
+let request = "odd"; //even
