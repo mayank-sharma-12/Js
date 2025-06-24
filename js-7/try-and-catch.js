2bb3c2a4a1bd27789af8f51265f8446482b657c4
Mayank Sharma <mayank.consurgys@gmail.com>
2025-06-24 16:01:29 +0530

    Add new files for practice

---
+++ js-7/try-and-catch.js
@@ -0,0 +1,11 @@
+console.log("Hello");
+console.log("Hello");
+// let a = "Fine working now!";
+try{
+    console.log(a);
+}
+catch{
+    console.log("It is working now by using try and catch!");
+}
+console.log("Hello world!");
+console.log("Hello world!");
